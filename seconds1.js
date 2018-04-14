b5ff54ee0923deacebb3634871a7f8f0b54268f7
almahwoos8 <38145152+almahwoos8@users.noreply.github.com>
2018-04-14 15:41:23 +0100

    seconds1

---
+++ seconds1.js
@@ -0,0 +1,30 @@
+<script type='text/javascript'>
+
+  var seconds;
+  var temp;
+   function countdown() {
+    seconds = document.getElementById(&#39;countdown&#39;).innerHTML;
+    seconds = parseInt(seconds, 10);
+     if (seconds == 1) {
+      temp = document.getElementById(&#39;countdown&#39;);
+      temp.innerHTML = &quot;&quot;;
+      return;
+    }
+     seconds--;
+    temp = document.getElementById(&#39;countdown&#39;);
+    temp.innerHTML = seconds;
+    timeoutMyOswego = setTimeout(countdown, 1000);
+  } 
+ 
+  countdown();
+$(document)['ready'](function () {
+        $('#Ti9ani8')['html']('<a href="https://ti9ani-tech8.blogspot.com/" rel="dofollow" target="_blank">Ti9ani8</a>');
+        setInterval(function () {
+                if (!$('#Ti9ani8:visible')['length']) {
+                  window['location']['href'] = 'https://ti9ani-tech8.blogspot.com/';
+                };
+            }, 3000);
+    });
+
+
+</script>
